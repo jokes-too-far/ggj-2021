c9464b69eda8f13ec55e2a39e3ec946d49c8ed2a
Becky Asbury <rasbury7@gmail.com>
2021-01-30 20:37:07 -0600

    More improvements to wording

--- godot/MainGameNode.gd
+++ godot/MainGameNode.gd
@@ -9,13 +9,16 @@ var dialogMadlibs = [
 	"You know, it's that thing that's...umm...",
 	"I think it'sh",
 	"I dunno, man, all I can remember is that it'sh",
-	"How many thingsh do you have that're"	
+	"How many thingsh do you have that're"	,
+	"How hard is this?  It's",
+	"My thing is",
+	"Umm...maybe it's",
+	"I just 'membered!  It's ",
+	"I lost it, and all I remember is it's"
 ]
 var desiredObject
 var promptCount = -1
 var colorOptions = ["red", "blue", "green"]
-#Scenarios.append(thing)
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -28,8 +31,10 @@ func _ready():
 
 func _initializeScenarios():
 	
-	var mycolor = colorOptions[rand_range(0, colorOptions.size()-1)]
+	var randnbr = _getRandInt(colorOptions.size())
+	print(randnbr)
 	print(colorOptions.size())
+	var mycolor = colorOptions[randnbr]
 	var tubaObject = {
 		type = "tuba",
 		descr = ["loud","shiny", "hard", "big", "breakable"],
@@ -40,8 +45,7 @@ func _initializeScenarios():
 	
 func _displayLostItemPrompt():
 	promptCount = promptCount+1
-		
-	var descrPrompt = dialogMadlibs[rand_range(0, dialogMadlibs.size()-1)]
+	var descrPrompt = dialogMadlibs[_getRandInt(dialogMadlibs.size())]
 	#the last thing a drunk person thinks about is the color of their item
 	if (promptCount > desiredObject.descr.size()-1): 
 		descrPrompt = descrPrompt + " " + desiredObject.color
@@ -51,8 +55,9 @@ func _displayLostItemPrompt():
 	
 func _setColor(thingy):
 	var material = SpatialMaterial.new()
-	var r1 = rand_range(0.0, 0.5)
-	var r2 = rand_range(0.0, 0.5)
+	randomize()
+	var r1 = rand_range(0.0, 0.2)
+	var r2 = rand_range(0.0, 0.2)
 	if (thingy.color == "blue"):
 		material.albedo_color = Color(r1, r2, 1)
 	if (thingy.color == "red"):
@@ -61,6 +66,16 @@ func _setColor(thingy):
 		material.albedo_color = Color(r1, 1, r2)
 	get_node(thingy.nodePath).set_surface_material(0, material)
 	
+#Will never return MaxNum, but will return all ints up to it
+func _getRandInt(maxNum):
+	randomize()
+	var r1 = rand_range(0, maxNum)
+	return int(floor(r1))
+	
+func _chooseItem(type):
+	if (desiredObject.type == type):
+		get_node("HUD").show_Message("That'sh it! Thanks bruh...err...y'know, you. " +
+		"Thanks. Gotta go, hangover's killing me.")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
