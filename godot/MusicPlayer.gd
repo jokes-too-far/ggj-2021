0a103f5e668cc6079c4ef6701df051b6572e6166
Becky Asbury <rasbury7@gmail.com>
2021-01-31 15:50:54 -0600

    the good news, is the music plays. the bad news is, we can't control it

---
+++ godot/MusicPlayer.gd
@@ -0,0 +1,19 @@
+extends AudioStreamPlayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _stopit():
+		self.stop()
+		#player.volume_db = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
